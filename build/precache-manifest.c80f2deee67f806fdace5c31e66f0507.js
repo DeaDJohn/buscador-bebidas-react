38fff7270f2439634672cb6f3a1e52b4279dcd31
DeaDJohn <juanjofb86@gmail.com>
2020-01-17 10:57:00 +0100

    subir el build

---
+++ build/precache-manifest.c80f2deee67f806fdace5c31e66f0507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0fa263cf3bc419360f22490625ab591",
+    "url": "/index.html"
+  },
+  {
+    "revision": "940d14dc61bfe3845741",
+    "url": "/static/css/main.02733e77.chunk.css"
+  },
+  {
+    "revision": "a2cd718022a560c0d4b2",
+    "url": "/static/js/2.4f5452a0.chunk.js"
+  },
+  {
+    "revision": "6ed4837c9741c870d03db3db11c80b52",
+    "url": "/static/js/2.4f5452a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "940d14dc61bfe3845741",
+    "url": "/static/js/main.107e78d0.chunk.js"
+  },
+  {
+    "revision": "7d2c1c6705ca85ba58c6",
+    "url": "/static/js/runtime-main.4f3dd3e6.js"
+  }
+]);
